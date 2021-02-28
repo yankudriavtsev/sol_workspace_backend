19fb0d9469d22566208c2deae9094a8e90a68045
Yan Kudriavtsev <yan.kudrivtsev@gmail.com>
2021-02-28 23:47:54 +0200

    frontend init

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Clementine</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
